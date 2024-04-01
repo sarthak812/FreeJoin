0da02b448ffdfc1fab481af9d0119291f17646a9
rahul <rahul.mittal@unb.ca>
2024-04-01 17:24:29 -0300

    Initial Adapter Class

--- src/main/java/org/example/adapter/FreeJoinCalciteAdapter.java
+++ src/main/java/org/example/adapter/FreeJoinCalciteAdapter.java
@@ -1,43 +1,251 @@
-package org.example.adapter;
+import org.apache.calcite.adapter.java.AbstractQueryableTable;
+import org.apache.calcite.linq4j.AbstractEnumerable;
+import org.apache.calcite.linq4j.Enumerable;
+import org.apache.calcite.linq4j.EnumerableDefaults;
+import org.apache.calcite.linq4j.Enumerator;
+import org.apache.calcite.rel.type.RelDataType;
+import org.apache.calcite.rel.type.RelDataTypeFactory;
+import org.apache.calcite.schema.AbstractSchema;
+import org.apache.calcite.schema.DataContext;
+import org.apache.calcite.schema.Schema;
+import org.apache.calcite.schema.SchemaFactory;
+import org.apache.calcite.schema.SchemaPlus;
+import org.apache.calcite.schema.Table;
+import org.apache.calcite.schema.TranslatableTable;
+import org.apache.calcite.sql.type.SqlTypeName;
+import org.bson.Document;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
 
- public class FreeJoinCalciteAdapter implements SchemaFactory {
+class Tuple {
+    List<Long> data;
 
-    // This method is called by Calcite to add a schema to the root schema.
+    public Tuple(List<Long> data) {
+        this.data = data;
+    }
+
+    // Implement getVariables method
+    public String getVariables(String[] trieVars) {
+        StringBuilder variables = new StringBuilder();
+        for (String var : trieVars) {
+            variables.append(var).append(",");
+        }
+        return variables.toString();
+    }
+}
+
+class Plan {
+    List<String> relations;
+
+    public Plan(List<String> relations) {
+        this.relations = relations;
+    }
+
+    // Implement contains method
+    public boolean contains(String relation) {
+        return relations.contains(relation);
+    }
+
+    // Implement getVars method
+    public String[] getVars() {
+        return relations.toArray(new String[0]);
+    }
+}
+
+class COLT {
+    String relation;
+    List<String> schema;
+    String vars;
+    COLTData data;
+
+    public COLT(String relation, List<String> schema) {
+        this.relation = relation;
+        this.schema = schema;
+        this.vars = schema.get(0);
+        List<Long> data = new ArrayList<>();
+        for (int i = 0; i < relation.length(); i++) {
+            data.add((long) i);
+        }
+        this.data = new COLTDataVec(data);
+    }
+
+    public Iterator<List<Long>> iter() {
+        return data.iter();
+    }
+
+    public COLT get(Tuple key) {
+        return data.get(key);
+    }
+
+    public void force() {
+        data.force(this);
+    }
+}
+
+interface COLTData {
+    Iterator<List<Long>> iter();
+
+    COLT get(Tuple key);
+
+    void force(COLT colt);
+}
+
+class COLTDataMap implements COLTData {
+    Map<Tuple, COLT> map;
+
+    public COLTDataMap() {
+        this.map = new HashMap<>();
+    }
+
+    @Override
+    public Iterator<List<Long>> iter() {
+        return map.keySet().stream()
+                .map(tuple -> tuple.data)
+                .iterator();
+    }
+
+    @Override
+    public COLT get(Tuple key) {
+        return map.get(key);
+    }
+
+    @Override
+    public void force(COLT colt) {
+        // Already in Map form, do nothing
+    }
+}
+
+class COLTDataVec implements COLTData {
+    List<List<Long>> vec;
+
+    public COLTDataVec(List<Long> data) {
+        this.vec = new ArrayList<>();
+        vec.add(data);
+    }
+
+    @Override
+    public Iterator<List<Long>> iter() {
+        return vec.iterator();
+    }
+
+    @Override
+    public COLT get(Tuple key) {
+        // Should not reach here
+        return null;
+    }
+
+    @Override
+    public void force(COLT colt) {
+        Map<Tuple, COLT> map = new HashMap<>();
+        for (int i = 0; i < vec.size(); i++) {
+            List<Long> cols = colt.relation.chars().mapToObj(c -> (long) c).collect(Collectors.toList());
+            Tuple k = new Tuple(cols);
+            if (!map.containsKey(k)) {
+                map.put(k, new COLT(colt.relation, colt.schema.subList(1, colt.schema.size())));
+            }
+            map.get(k).data = new COLTDataVec(new ArrayList<>());
+            map.get(k).data.vec.add(vec.get(i));
+        }
+        colt.data = new COLTDataMap();
+        ((COLTDataMap) colt.data).map = map;
+    }
+}
+
+public class FreeJoinCalciteAdapter implements SchemaFactory {
+
     @Override
     public Schema create(SchemaPlus parentSchema, String name, Map<String, Object> operand) {
         return new FreeJoinSchema();
     }
 
-    // Represents a schema in the FreeJoin context.
     public static class FreeJoinSchema extends AbstractSchema {
         private final Map<String, Table> tableMap = new HashMap<>();
 
         @Override
         protected Map<String, Table> getTableMap() {
-            // Lazy initialization of tables
-            // For example, if your Free Join tables are based on CSV files:
-            tableMap.putIfAbsent("MyFreeJoinTable", new FreeJoinTable("path/to/csvfile.csv"));
-            // Add additional tables as needed
+            tableMap.putIfAbsent("MyFreeJoinTable", new FreeJoinTable());
             return tableMap;
         }
     }
 
-    // Represents a table in the FreeJoin context.
-    // This is a placeholder for the actual implementation.
-    public static class FreeJoinTable implements Table {
-        private final String dataSource;
+    public static class FreeJoinTable extends AbstractQueryableTable implements TranslatableTable {
+        private final String mongoUri = "mongodb://localhost:27017";
+        private final String dbName = "your_database_name";
+        private final String collectionName = "your_collection_name";
 
-        public FreeJoinTable(String dataSource) {
-            this.dataSource = dataSource;
+        public FreeJoinTable() {}
+
+        private MongoCollection<Document> getCollection() {
+            MongoClient mongoClient = MongoClients.create(mongoUri);
+            MongoDatabase database = mongoClient.getDatabase(dbName);
+            return database.getCollection(collectionName);
         }
-//
-//        // You will need to implement Table methods such as getRowType to provide metadata
-//        // and implement enumerable on top of your data source, following Calcite conventions.
-//
+
         @Override
-        public Expression getExpression(SchemaPlus schema, String tableName, Class clazz) {
-            // Implementation to return the expression that represents this table.
-            return null;
+        public RelDataType getRowType(RelDataTypeFactory typeFactory) {
+            RelDataTypeFactory.Builder builder = new RelDataTypeFactory.Builder(typeFactory);
+            builder.add("id", SqlTypeName.INTEGER);
+            builder.add("name", SqlTypeName.VARCHAR);
+            // Add more columns as needed
+            return builder.build();
+        }
+
+        @Override
+        public Enumerable<Object[]> scan(DataContext root) {
+            return new AbstractEnumerable<Object[]>() {
+                public Enumerator<Object[]> enumerator() {
+                    List<Object[]> rows = new ArrayList<>();
+                    MongoCollection<Document> collection = getCollection();
+                    collection.find().forEach((Consumer<Document>) document -> {
+                        Object[] row = new Object[]{document.getInteger("id"), document.getString("name")};
+                        rows.add(row);
+                    });
+                    return EnumerableDefaults.nonNullEnumerator(rows.iterator());
+                }
+            };
+        }
+
+        public void join(List<COLT> allTries, List<Plan> plan, Tuple tuple) {
+            if (plan.isEmpty()) {
+                output(tuple); // Implement the output method or replace with appropriate logic
+            } else {
+                List<COLT> tries = new ArrayList<>();
+                for (COLT t : allTries) {
+                    if (plan.get(0).contains(t.relation)) {
+                        tries.add(t);
+                    }
+                }
+
+                for (Iterator<List<Long>> iter = tries.get(0).iter(); iter.hasNext(); ) {
+                    List<Long> t = iter.next();
+                    List<COLT> subTries = new ArrayList<>();
+                    Tuple newTuple = new Tuple(t); // Adjust Tuple constructor as needed
+
+                    for (int i = 1; i < tries.size(); i++) {
+                        Plan triePlan = plan.get(i);
+                        String[] trieVars = triePlan.getVars(); // Implement getVars() method in Plan class
+                        String key = newTuple.getVariables(trieVars); // Implement getVariables() method in Tuple class
+                        COLT subTrie = tries.get(i).get(newTuple);
+                        if (subTrie == null) {
+                            continue;
+                        }
+                        subTries.add(subTrie);
+                    }
+
+                    List<COLT> newTries = new ArrayList<>(allTries);
+                    newTries.replaceAll(t -> subTries.contains(t) ? t : null);
+                    join(newTries, plan.subList(1, plan.size()), newTuple);
+                }
+            }
         }
     }
 }
